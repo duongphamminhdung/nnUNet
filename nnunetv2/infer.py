a9ebfee5fa21905f8999a7353f018b112db8d2af
Duong Pham Minh Dung <94067220+duongphamminhdung@users.noreply.github.com>
2024-12-03 20:22:05 +0700

    change infer.py and add infer to pyproject.toml

--- nnunetv2/infer.py
+++ nnunetv2/infer.py
@@ -31,21 +31,73 @@ def convert_mask_from_folder(folder_path):
 def predict_1_file(path):
     pass
 
-def predict(input_folder, output_folder, model_folder, fold='all', step_size=0.5, disable_tta=False, verbose=False,
-                                    save_probabilities=False, continue_prediction=False, chk='checkpoint_final.pth', npp=3,
-                                    nps=3, prev_stage_predictions=None, device='cuda', disable_progress_bar=False):
+def infer():
+    import argparse
+    parser = argparse.ArgumentParser(description='Use this to run inference with nnU-Net. This function is used when '
+                                                 'you want to manually specify a folder containing a trained nnU-Net '
+                                                 'model. This is useful when the nnunet environment variables '
+                                                 '(nnUNet_results) are not set.')
+    parser.add_argument('-i', type=str, required=True,
+                        help='input folder. Remember to use the correct channel numberings for your files (_0000 etc). '
+                             'File endings must be the same as the training dataset!')
+    parser.add_argument('-o', type=str, required=True,
+                        help='Output folder. If it does not exist it will be created. Predicted segmentations will '
+                             'have the same name as their source images.')
+    parser.add_argument('-m', type=str, required=True,
+                        help='Folder in which the trained model is. Must have subfolders fold_X for the different '
+                             'folds you trained')
+    parser.add_argument('-f', nargs='+', type=str, required=False, default=(0, 1, 2, 3, 4),
+                        help='Specify the folds of the trained model that should be used for prediction. '
+                             'Default: (0, 1, 2, 3, 4)')
+    parser.add_argument('-step_size', type=float, required=False, default=0.5,
+                        help='Step size for sliding window prediction. The larger it is the faster but less accurate '
+                             'the prediction. Default: 0.5. Cannot be larger than 1. We recommend the default.')
+    parser.add_argument('--disable_tta', action='store_true', required=False, default=False,
+                        help='Set this flag to disable test time data augmentation in the form of mirroring. Faster, '
+                             'but less accurate inference. Not recommended.')
+    parser.add_argument('--verbose', action='store_true', help="Set this if you like being talked to. You will have "
+                                                               "to be a good listener/reader.")
+    parser.add_argument('--save_probabilities', action='store_true',
+                        help='Set this to export predicted class "probabilities". Required if you want to ensemble '
+                             'multiple configurations.')
+    parser.add_argument('--continue_prediction', '--c', action='store_true',
+                        help='Continue an aborted previous prediction (will not overwrite existing files)')
+    parser.add_argument('-chk', type=str, required=False, default='checkpoint_final.pth',
+                        help='Name of the checkpoint you want to use. Default: checkpoint_final.pth')
+    parser.add_argument('-npp', type=int, required=False, default=3,
+                        help='Number of processes used for preprocessing. More is not always better. Beware of '
+                             'out-of-RAM issues. Default: 3')
+    parser.add_argument('-nps', type=int, required=False, default=3,
+                        help='Number of processes used for segmentation export. More is not always better. Beware of '
+                             'out-of-RAM issues. Default: 3')
+    parser.add_argument('-prev_stage_predictions', type=str, required=False, default=None,
+                        help='Folder containing the predictions of the previous stage. Required for cascaded models.')
+    parser.add_argument('-device', type=str, default='cuda', required=False,
+                        help="Use this to set the device the inference should run with. Available options are 'cuda' "
+                             "(GPU), 'cpu' (CPU) and 'mps' (Apple M1/M2). Do NOT use this to set which GPU ID! "
+                             "Use CUDA_VISIBLE_DEVICES=X nnUNetv2_predict [...] instead!")
 
-    if not isdir(output_folder):
-        maybe_mkdir_p(output_folder)
+    print(
+        "\n#######################################################################\nPlease cite the following paper "
+        "when using nnU-Net:\n"
+        "Isensee, F., Jaeger, P. F., Kohl, S. A., Petersen, J., & Maier-Hein, K. H. (2021). "
+        "nnU-Net: a self-configuring method for deep learning-based biomedical image segmentation. "
+        "Nature methods, 18(2), 203-211.\n#######################################################################\n")
 
-    assert device in ['cpu', 'cuda',
+    args = parser.parse_args()
+    args.f = [i if i == 'all' else int(i) for i in args.f]
+
+    if not isdir(args.o):
+        maybe_mkdir_p(args.o)
+
+    assert args.device in ['cpu', 'cuda',
                            'mps'], f'-device must be either cpu, mps or cuda. Other devices are not tested/supported. Got: {args.device}.'
-    if device == 'cpu':
+    if args.device == 'cpu':
         # let's allow torch to use hella threads
         import multiprocessing
         torch.set_num_threads(multiprocessing.cpu_count())
         device = torch.device('cpu')
-    elif device == 'cuda':
+    elif args.device == 'cuda':
         # multithreading in torch doesn't help nnU-Net if run on GPU
         torch.set_num_threads(1)
         torch.set_num_interop_threads(1)
@@ -53,26 +105,28 @@ def predict(input_folder, output_folder, model_folder, fold='all', step_size=0.5
     else:
         device = torch.device('mps')
 
-    predictor = nnUNetPredictor(tile_step_size=step_size,
+    predictor = nnUNetPredictor(tile_step_size=args.step_size,
                                 use_gaussian=True,
-                                use_mirroring=not disable_tta,
+                                use_mirroring=not args.disable_tta,
                                 perform_everything_on_device=True,
                                 device=device,
-                                verbose=verbose,
-                                allow_tqdm=not disable_progress_bar,
-                                verbose_preprocessing=verbose)
-    predictor.initialize_from_trained_model_folder(model_folder, fold, chk)
-    predictor.predict_from_files(input_folder, output_folder, save_probabilities=save_probabilities,
-                                 overwrite=not continue_prediction,
-                                 num_processes_preprocessing=npp,
-                                 num_processes_segmentation_export=nps,
-                                 folder_with_segs_from_prev_stage=prev_stage_predictions,
+                                verbose=args.verbose,
+                                allow_tqdm=not args.disable_progress_bar,
+                                verbose_preprocessing=args.verbose)
+    predictor.initialize_from_trained_model_folder(args.m, args.f, args.chk)
+    predictor.predict_from_files(args.i, args.o, save_probabilities=args.save_probabilities,
+                                 overwrite=not args.continue_prediction,
+                                 num_processes_preprocessing=args.npp,
+                                 num_processes_segmentation_export=args.nps,
+                                 folder_with_segs_from_prev_stage=args.prev_stage_predictions,
                                  num_parts=1, part_id=0)
-    convert_mask_from_folder(output_folder)
+    
+    print("Converting mask")
+    convert_mask_from_folder(args.o)
 
 # if __name__ == "__main__":
 #     predict()
-# !nnUNet_results="/content/drive/MyDrive/AI_TRAINING/signal-ecg/results" \
+# nnUNet_results="/content/drive/MyDrive/AI_TRAINING/signal-ecg/results" \
 # nnUNetv2_predict \
 # -i input_folder \
 # -o output_folder \
